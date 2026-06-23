dc5aff2a51d14c77b6c81dbb502a6864c61550e7
Eitan Dror <ethandror123@gmail.com>
2026-06-23 21:06:53 +0300

    fixed hood reset sequence

--- src/main/java/frc/robot/subsystems/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/hood/Hood.java
@@ -55,6 +55,8 @@ public class Hood extends MotorSubsystem<MotorInputsAutoLogged, MotorIO> {
   @AutoLogOutput(key = "Hood/currentState")
   private HoodStates currentState = HoodStates.IDLE;
 
+  private HoodStates previousState = HoodStates.IDLE;
+
   @AutoLogOutput(key = "Hood/targetAngle")
   private DoubleSupplier targetAngle = ()->0.0;
 
@@ -154,7 +156,7 @@ public class Hood extends MotorSubsystem<MotorInputsAutoLogged, MotorIO> {
           targetAngle.getAsDouble(), MOTOR_FF.calculate(super.inputs.unitPosition, super.inputs.velocityUnitsPerSecond));
       case CLOSED -> super.setMaxMotionSetpointPosition(HOOD_MAX_ANGLE, HOOD_ANGLE_TOLERANCE);
       case BOOT_SEQUENCE -> {
-        bootSequenceTimer.start();
+        if(previousState != currentState) bootSequenceTimer.start();
         if(bootSequenceTimer.hasElapsed(BOOT_SEQUENCE_TIME)) {
           hasBeenReset = true;
           resetPosition(HOOD_STARTING_ANGLE);
@@ -168,6 +170,7 @@ public class Hood extends MotorSubsystem<MotorInputsAutoLogged, MotorIO> {
   }
 
   public void setState(HoodStates wantedState) {
+    previousState = currentState;
     if(wantedState == HoodStates.BOOT_SEQUENCE && currentState != HoodStates.BOOT_SEQUENCE) 
       startBootSequence();
     else if (currentState == HoodStates.BOOT_SEQUENCE && wantedState != HoodStates.BOOT_SEQUENCE) 
