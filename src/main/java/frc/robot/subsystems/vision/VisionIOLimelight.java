65eab38aae950604dc1549f4a5ec94a6e5111e2f
Eitan Dror <ethandror123@gmail.com>
2026-07-23 17:21:08 +0300

    .

--- src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOLimelight.java
@@ -97,7 +97,7 @@ public class VisionIOLimelight implements VisionIO {
       if(rawResult.value[9] <= 6.0 && rawResult.value[7] > 1) {
         RobotState.getInstance().addLimelightYawObservation(
           new LimelightYawObservation(
-            new Rotation2d(parsePose(rawResult.value).getRotation().getZ()), 
+            parsePose(rawResult.value).getRotation().toRotation2d(), 
             rawResult.timestamp * (1.0e-6)
             ));
       }
